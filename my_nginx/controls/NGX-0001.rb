184e6f1af4be3c32a0c58f8940666484b897a015
Michael Dailous <github@dailous.net>
2024-08-22 22:08:52 +0000

    fix: replace "summary" with "desc" in NGX-0001

--- my_nginx/controls/NGX-0001.rb
+++ my_nginx/controls/NGX-0001.rb
@@ -4,7 +4,7 @@
 control "NGX-0001" do                        # A unique ID for this control
   impact 1.0                                # The criticality, if this control fails.
   title "Verify NGINX installation."  
-  summary "Make sure NGINX is installed. If it is not, nothing else will work."
+  desc "Make sure NGINX is installed. If it is not, nothing else will work."
   desc 'fix', "Install NGINX version #{input('nginx_version')} or greater."  
   tag severity: 'high'
   
